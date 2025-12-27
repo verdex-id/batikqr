dfad07f2ef97910a5fbbcc03a0f571d9f84857d2
unknown <agilistikmal3@gmail.com>
2025-12-27 18:44:21 +0700

    error handler

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,11 @@ const app = new Hono();
 // Mount static files
 app.use("/asset/*", serveStatic({ root: "./" }));
 
+app.onError((err, c) => {
+    console.error(err);
+    return c.json({ error: err.message }, 500);
+});
+
 app.route("/pattern", patternHandler);
 
 export default app;
